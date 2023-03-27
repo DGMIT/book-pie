42aefbaef8feb4d45e0e3f389ee42db56ce20c73
Yoong-Lee <yoong.lee@dgmit.com>
2023-03-28 02:50:14 +0900

    feat: 목록 버튼 해더로 위치 이동

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,8 +1,18 @@
-const Header = () => {
+import { Link, useLocation } from "react-router-dom";
 
+const Header = () => {
+    const location = useLocation();
 
     return (
-        <h1>BOOK PIE</h1>
+        <header>
+            <h1>BOOK PIE</h1>
+            {location.pathname.indexOf('/report') !== -1 ? 
+                <button>
+                    <Link to="/">책 목록으로</Link>
+                </button>
+                : null
+            }
+        </header>
     )
 }
 
