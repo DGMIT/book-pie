8e6ba5e1ef01ed67e0add93b2e539ba6944928cc
Yoong-Lee <yoong.lee@dgmit.com>
2023-03-28 21:05:55 +0900

    feat: 독후감 리스트 조회 API 적용

--- client/src/components/report/ReportList.tsx
+++ client/src/components/report/ReportList.tsx
@@ -1,11 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Report } from "../../models/report.model";
 import ReportBox from "./ReportBox";
 
-const ReportList = () => {
+const ReportList = ({bookId}: {bookId: string}) => {
+    const [isError, setIsError] = useState<boolean>(false);
+    const [reportList, setReportList] = useState<Report[]>();
+    //전체 독후감 리스트 받아오기
+    const handleFetch = () => {
+        axios
+            .get("http://localhost:4000/report/" + bookId)
+            .then((response) => {
+                const data = response.data;
+                if(data.result === 'OK') {
+                    setReportList(data.reportList);
+                    setIsError(false);
+                }
+            })
+            .catch((error) => {
+                setIsError(true);
+            });
+    };
+
+    useEffect(() => {
+        handleFetch();
+    }, []);
+    
     return (
         <section>
-            <ReportBox mode='edit'/>
-            {new Array(3).fill(0).map((el, idx) => (
-                <ReportBox mode="readonly" key={idx}/>
+            <ReportBox />
+            {reportList && reportList.map((data: Report) => (
+                <li key={data.bookReportId}>
+                    <ReportBox data={data}/>
+                </li>
             ))}
         </section>
     )
