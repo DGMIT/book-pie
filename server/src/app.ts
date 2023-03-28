f677718765311a86924bc020be7779da57b75f2f
Yoong-Lee <yoong.lee@dgmit.com>
2023-03-28 19:12:57 +0900

    feat: 컨트롤러 임포트

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,8 +4,9 @@ import * as cors from 'cors';
 import * as mysql from 'mysql2';
 import { InversifyExpressServer } from 'inversify-express-utils';
 import container from './config/ioc.container';
-import "./controller/book.controller"
-
+//컨트롤러 바인딩은 필요 없으나, import는 필요
+import './controller/book.controller';
+import './controller/report.controller';
 
 const server = new InversifyExpressServer(container);
 
