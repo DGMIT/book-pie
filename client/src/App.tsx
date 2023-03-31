7b3c44edfb98fdc493206a3877916e923a56c682
Yoong-Lee <yoong.lee@dgmit.com>
2023-03-31 16:16:34 +0900

    feat: 공통 컴포넌트 위치 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css';
-import Header from './components/Header';
+import Header from './components/common/Header';
 import BookPage from './pages/BookPage';
 import ReportPage from './pages/ReportPage';
 
