6ffd6167e7c587b32956babc1b45d5b26d26efa4
Yoong-Lee <yoong.lee@dgmit.com>
2023-04-03 11:49:29 +0900

    design: 헤더 스타일링

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -1,18 +1,35 @@
 import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import { StyledSubLinkBtn } from "../../styled/StyledBtn";
+
+const StyledHeader = styled.header`
+    display: flex;
+    justify-content: space-between; 
+    padding: 20px 0;
+    > h1 {
+        font-size: 34px;
+        color: var(--point-color);
+    }
+
+    .button-box {
+        padding-top: 4px;
+    }
+`;
+
 
 const Header = () => {
     const location = useLocation();
 
     return (
-        <header>
+        <StyledHeader>
             <h1>BOOK PIE</h1>
             {location.pathname.indexOf('/report') !== -1 ? 
-                <div className="button-box">
+                <StyledSubLinkBtn className="button-box">
                     <Link to="/">책 목록으로</Link>
-                </div>
+                </StyledSubLinkBtn>
                 : null
             }
-        </header>
+        </StyledHeader>
     )
 }
 
